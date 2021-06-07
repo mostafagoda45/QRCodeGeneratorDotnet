46d081561ff14eefd03a330bde96a107ab43cdeb
MostafaGoda <mostafagoda1993@gmail.com>
2021-06-07 17:19:55 +0200

    Add initial code

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using QRCoder;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QRCodePOC.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        [Route("qrcode/generate")]
+        public async Task<IActionResult> GenerateQRCode(string text)
+        {
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(text, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+            BitmapByteQRCode bitmapByteQRCode = new BitmapByteQRCode(qrCodeData);
+            return File(bitmapByteQRCode.GetGraphic(25), "image/bmp");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-QRCodePOC
+# QRCodePOC
+
+This repository is used for generating QRCode image from text using `Dotnet Core` library [QRCoder](https://github.com/codebude/QRCoder) 
